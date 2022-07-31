db4d46868cdc8a0942f43d230edb77a0af30d6b7
Mohammad Irfan <mohdirfan57@gmail.com>
2022-07-31 14:34:23 +0530

    this is via git push next

--- mongotest.py
+++ mongotest.py
@@ -9,6 +9,30 @@ d = {
     "email":"mohai@ineea.com"
 }
 
+d = {
+    "name":"maverick",
+    "surname":"vinales",
+    "email":"maverick@moto.com"
+}
+
+d = {
+    "name":"eden",
+    "surname":"hazard",
+    "email":"hazard7@real.com"
+}
+
+d = {
+    "name":"toni",
+    "surname":"kroos",
+    "email":"toni@real.com"
+}
+
+d = {
+    "name":"kairm",
+    "surname":"benzema",
+    "email":"karim@madr.com"
+}
+
 db1 = client['mongodb']
 coll = db1['test']
 coll.insert_one(d)
